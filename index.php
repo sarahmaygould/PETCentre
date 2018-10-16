68b1a7f21bc47520e69030e6b4cc340aa1826d1c
Sarah-May Gould <sarahmayg@pet>
2018-10-16 10:19:14 +0100

    Working on home page.

--- index.php
+++ index.php
@@ -15,7 +15,33 @@
       <?php include_once 'Header/header.html'; ?>
 
       <div id="page-body">
-        PAGE BODY
+        <div id="top-section-container">
+          <div id="image-container">
+            <img class="homepage-image" src="Index/images/reception_small.jpg" alt="PET Centre Reception" height="200">
+            <img class="homepage-image" src="Index/images/PETCT_Emma_small.jpg" alt="PET-CT Scanner with patient" height="200">
+          </div>
+          <div id="text-container">
+            <p>
+              With more than 20 years experience of PET and PET-CT scanning and now also PET-MR, the King&#39;s College London & Guy&#39;s and St Thomas&#39; PET Centre provides PET services for referred patients at a local and national level and performs clinically related research. We scan over 6000 patients per year, and the enhancements of our newly refurbished centre let us be there for our patients at every step of the experience and make it as smooth and comfortable as possible.
+            </p>
+            <p>
+              Our PET Centre is a centre of excellence with an international reputation for delivering the best patient care, highest quality academic research, and training. We strive to make a difference for the patients we scan, and via the research we perform.
+            </p>
+            <p>
+              <b>
+                Our mission is to perform basic, translational and applied clinical PET research across all specialties including oncology, neurology/psychiatry, and cardiology. We welcome work with external collaborators, including industry. We aim to obtain the best possible quality and reliability for all studies we perform.
+              </b>
+            </p>
+            <p>
+              <b>
+                The Centre develops exciting PET methodology (e.g. PET-MR), and also performs cutting-edge research into innovative radiochemistry using our in-house cyclotron and our wide range of multidisciplinary expertise.
+              </b>
+            </p>
+          </div>
+        </div>
+
+        <hr>
+
       </div>
     </div>
   </body>
