1fe6480fb529d27c4e5c636303741b122e3bd4c1
Sarah-May Gould <sarahmayg@pet>
2018-10-16 09:07:43 +0100

    Converted to php.

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="Header/header-style.css">
+    <link rel="stylesheet" type="text/css" href="Index/index-style.css">
+    <title>The PET Centre</title>
+  </head>
+
+  <body>
+    <div class = "page-content">
+
+      <?php include_once 'Header/header.html'; ?>
+
+      <div id="page-body">
+        PAGE BODY
+      </div>
+    </div>
+  </body>
+
+</html>
