d97d3b64b98cd24ea0be0f5c81f6ae9427fca357
Sarah-May Gould <sarahmayg@pet>
2018-10-17 12:18:02 +0100

    Making about us work still.

--- Header/header.php
+++ Header/header.php
@@ -1,17 +1,17 @@
 <html>
 <div id="page-header">
   <div class="logo-containers" id="pet-logo-container">
-    <a href="index.php"><img class="logos" id="logo-pet" src="Header/images/logo_pet.jpg"  alt="PET Imaging Centre logo"></a>
+    <a href="index.php"><img class="logos" id="logo-pet" src="/Header/images/logo_pet.jpg"  alt="PET Imaging Centre logo"></a>
   </div>
   <div class="logo-containers" id="other-logos-container">
     <div class="logo-containers" id="gstt-logo-container">
-      <a href="http://www.guysandstthomas.nhs.uk" target="_blank"><img class="logos" id="logo-gstt" src="Header/images/logo_gstt.jpg"  alt="Guy&#39;s and St Thomas' logo"></a>
+      <a href="http://www.guysandstthomas.nhs.uk" target="_blank"><img class="logos" id="logo-gstt" src="/Header/images/logo_gstt.jpg"  alt="Guy&#39;s and St Thomas' logo"></a>
     </div>
     <div class="logo-containers" id="gcc-logo-container">
-      <a href="http://www.guysandstthomas.nhs.uk/our-services/cancer/overview.aspx" target="_blank"><img class="logos" id="logo-gcc" src="Header/images/logo_gcc.jpg"  alt="Guy&#39;s Cancer logo"></a>
+      <a href="http://www.guysandstthomas.nhs.uk/our-services/cancer/overview.aspx" target="_blank"><img class="logos" id="logo-gcc" src="/Header/images/logo_gcc.jpg"  alt="Guy&#39;s Cancer logo"></a>
     </div>
     <div class="logo-containers" id="kcl-logo-container">
-      <a href="http://www.kcl.ac.uk" target="_blank"><img class="logos" id="logo-kcl" src="Header/images/logo_kcl.jpg"  alt="King&#39;s College London logo"></a>
+      <a href="http://www.kcl.ac.uk" target="_blank"><img class="logos" id="logo-kcl" src="/Header/images/logo_kcl.jpg"  alt="King&#39;s College London logo"></a>
     </div>
   </div>
 </div>
