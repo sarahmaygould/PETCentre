88328c967be3fd30467319c152963ca56b0fa10e
Sarah-May Gould <sarahmayg@pet>
2018-10-22 16:14:20 +0100

    Playing with php on news page.

--- News/news.php
+++ News/news.php
@@ -16,6 +16,7 @@
       $news_item_array = array_map('str_getcsv', file("news_items.csv"));
       unset($news_item_array[0]);
       $start_loop = 1;
+      $counter = 1;
     ?>
 
     <div class = "page-content">
@@ -28,17 +29,32 @@
         </div>
 
         <?php foreach ($news_item_array as $news_item) {
-          if ($start_loop==0) echo "<hr>"; ?>
+          if ($start_loop==0) echo "<hr>";
+          if ($counter % 2 == 0) {
+        ?>
           <div class="LR-container">
             <div class="LI-image">
               <img src="images/<?php echo $news_item[2]?>" alt="<?php echo $news_item[2]?>" width="150">
             </div>
-            <div class="LI-text staff-section">
+            <div class="LI-text">
               <h1><?php echo $news_item[0]?></h1>
               <?php echo $news_item[1]?>
             </div>
           </div>
-        <?php $start_loop = 0;
+        <?php } else {
+        ?>
+          <div class="LR-container">
+            <div class="RI-text">
+              <h1><?php echo $news_item[0]?></h1>
+              <?php echo $news_item[1]?>
+            </div>
+            <div class="RI-image">
+              <img src="images/<?php echo $news_item[2]?>" alt="<?php echo $news_item[2]?>" width="150">
+            </div>
+          </div>
+        <?php }
+          $start_loop = 0;
+          $counter++;
         } ?>
 
       </div>
