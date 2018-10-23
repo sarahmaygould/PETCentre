bb762add6d58ee2cc309d1ef476afbf8b46e22c5
Sarah-May Gould <sarahmayg@pet>
2018-10-23 09:13:05 +0100

    Set up courses page.

---
+++ Courses/courses.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="../Header/header-style.css">
+    <link rel="stylesheet" type="text/css" href="../common-style.css">
+    <title>The PET Centre</title>
+  </head>
+
+  <body>
+
+    <div class = "page-content">
+
+      <?php include_once '../Header/header.php'; ?>
+
+      <div class="page-body">
+        <div class="page-title">
+          Courses
+        </div>
+
+        <p>
+          The PET Centre at St Thomas&#39; Hospital London runs regular training courses for physicians, radiologists, radiographers and scientists and, as part of the medical school, contributes to undergraduate and postgraduate teaching.
+        </p>
+        <p>
+          If you are interested in one of our projects and becoming one of our PhDs, Post Docs or Fellows please look on the staff page for the relevant principal investigator's details and get in touch.
+        </p>
+        <p>
+          To look at other short course options that we have available please take a look <a href="http://www.kcl.ac.uk/lsm/research/divisions/imaging/study/Short-courses.aspx" target="_blank">here</a>.
+        </p>
+        <p>
+          Contact <a href="mailto:teachingadmin-imaging@kcl.ac.uk">teachingadmin-imaging@kcl.ac.uk</a> if you wish to be added to the course mailing list.
+        </p>
+
+        <hr>
+
+        <?php include_once 'read_in_courses.php'; ?>
+
+        <div class="LR-container">
+          <div class="LI-image">
+            <img src="images/LondonEye_small.jpg" alt="MSc Radiopharmaceuticals Course" width="150">
+          </div>
+          <div class="LI-text">
+            <h1>MSc and MRes in PET Radiochemistry</h1>
+            <p>
+              The Radiopharmaceutics & PET Radiochemistry MSc programme is the only one of its kind worldwide.
+            </p>
+            <p>
+              We offer relevant work placements alongside excellent teaching to lead you on your chosen pathway.
+            </p>
+            <p>
+              This course will equip you to work as a radiopharmaceutical scientist in a PET radiochemistry centre (cyclotron unit) or a conventional radiopharmacy.
+            </p>
+            <p>
+              <a href="http://www.kcl.ac.uk/study/postgraduate/taught-courses/radiopharmaceutics-and-pet-radiochemistry-msc-pg-dip-pg-cert.aspx" target="_blank">Click here for more information and to apply.</a>
+            </p>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="LR-container">
+          <div class="LI-image">
+            <img src="images/PETCT_small.png" alt="MSc Nuclear Medicine Course" width="150">
+          </div>
+          <div class="LI-text">
+            <h1>MSc in Nuclear Medicine</h1>
+            <p>
+                This programme develops skills for the provision of safe, high-quality nuclear medicine services by offering nuclear medicine training with a strong scientific and academic framework in an approved structured service environment.
+            </p>
+            <p>
+              The course is taught collaboratively by five University of London medical schools and draws on professional expertise from many disciplines giving you a solid base firmly grounded in reali life application of the subject.
+            </p>
+            <p>
+              This course will equip you to work as a radiopharmaceutical scientist in a PET radiochemistry centre (cyclotron unit) or a conventional radiopharmacy.
+            </p>
+            <p>
+              <a href="http://www.kcl.ac.uk/prospectus/graduate/nuclear-medicine-science-and-practice" target="_blank">Click here for more information and to apply.</a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+      <div class="page-end"></div>
+    </div>
+  </body>
+
+</html>
---
+++ Courses/read_in_courses.php
@@ -0,0 +1,34 @@
+<?php
+$course_array = array_map('str_getcsv', file("courses.csv"));
+unset($course_array[0]);
+$start_loop = 1;
+$counter = 1;
+
+foreach ($course_array as $news_item) {
+  if ($course[6] == "TRUE") {
+?>
+    <div class="LR-container">
+      <div class="LI-image">
+        <img src="<?php echo "images/" . $course[2]?>" alt="<?php echo $course[2]?>" width="150">
+      </div>
+      <div class="LI-text">
+        <h1><?php echo $course[0]?></h1>
+        <?php echo $course[1]?>
+
+        <p>
+          This course will next be held on <?php echo $course[5]?>.
+        </p>
+        <p>
+          <a href="<?php echo "documents/" . $course[3]?>" target="_blank">Click here to download the course flyer.</a>
+        </p>
+        <p>
+          <a href="<?php echo $course[4]?>" target="_blank">Click here for more information and to apply.</a>
+        </p>
+
+      </div>
+    </div>
+  <?php
+
+    echo "<hr>";
+  }
+} ?>
