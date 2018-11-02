6d0c63e02fc23d5a89f1fca8fd06d0f7e446c608
Sarah-May Gould <sarahmayg@pet>
2018-11-02 10:46:33 +0000

    Put in table for patient satisfaction surveys.

--- ForPatients_YourVisit/read_in_pss.php
+++ ForPatients_YourVisit/read_in_pss.php
@@ -8,18 +8,6 @@ foreach ($pss_array as $pss) {
     <p>
       <a class="PSS_download" href="documents/PatientSatisfactionSurvey_Q<?php echo $pss[0]?>_<?php echo $pss[1]?>.pdf">Quarter <?php echo $pss[0]?>: <?php echo $pss[2]?> - <?php echo $pss[3]?> <?php echo $pss[1]?></a>
     </p>
-        <p>
-          This course will next be held on <b><?php echo $pss[5]?></b>.
-        </p>
-        <p>
-          <a href="<?php echo "documents/" . $pss[3]?>" target="_blank">Click here to download the course flyer.</a>
-        </p>
-        <p>
-          <a href="<?php echo $pss[4]?>" target="_blank">Click here for more information and to apply.</a>
-        </p>
-
-      </div>
-    </div>
   <?php
   }
 } ?>
