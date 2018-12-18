198e8055d1c62b3eeb7e67e90abc46f61cbe3aa5
Sarah-May Gould <sarahmayg@pet>
2018-12-18 12:35:47 +0000

    Working on courses page.

--- Courses/read_in_courses.php
+++ Courses/read_in_courses.php
@@ -19,17 +19,16 @@ foreach ($course_array as $course) {
         <h1><?php echo $course[0]?></h1>
         <?php echo $course[1]?>
 
-        <p>
-          This course will next be held on <b><?php echo date_format($course_date, 'jS F Y')?></b>.
-        </p>
-
-        <?php if ($course[7] == "TRUE") { ?>
+        <?php if ($course_date > $today) { ?>
           <p>
-            <a href="pet_technology_course.php" target="_blank">Click here for more information about this course.</a>
+            This course will next be held on <b><?php echo date_format($course_date, 'jS F Y')?></b>.
           </p>
-        <?php }
 
-        if ($course_date > $today) { ?>
+          <?php if ($course[7] == "TRUE") { ?>
+            <p>
+              <a href="pet_technology_course.php" target="_blank">Click here for more information about this course.</a>
+            </p>
+          <?php } ?>
           <p>
             <a href="<?php echo "documents/" . $course[3]?>" target="_blank">Click here to download the course flyer.</a>
           </p>
