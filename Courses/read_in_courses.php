223ec2bac0fc72de35d57deea9e809e1600617c5
Sarah-May Gould <sarahmayg@pet>
2018-12-18 12:29:13 +0000

    Working on courses page.

--- Courses/read_in_courses.php
+++ Courses/read_in_courses.php
@@ -10,38 +10,36 @@ foreach ($course_array as $course) {
     var_dump($course_date);
     var_dump($today);
     var_dump($course_date > $today);
-
-    if ($course_date > $today) {
 ?>
-      <div class="LR-container">
-        <div class="LI-image">
-          <img src="<?php echo "images/" . $course[2]?>" alt="<?php echo $course[2]?>" width="150">
-        </div>
-        <div class="LI-text">
-          <h1><?php echo $course[0]?></h1>
-          <?php echo $course[1]?>
+    <div class="LR-container">
+      <div class="LI-image">
+        <img src="<?php echo "images/" . $course[2]?>" alt="<?php echo $course[2]?>" width="150">
+      </div>
+      <div class="LI-text">
+        <h1><?php echo $course[0]?></h1>
+        <?php echo $course[1]?>
 
+        <p>
+          This course will next be held on <b><?php echo date_format($course_date, 'jS F Y')?></b>.
+        </p>
+
+        <?php if ($course[7] == "TRUE") { ?>
           <p>
-            This course will next be held on <b><?php echo date_format($course_date, 'jS F Y')?></b>.
+            <a href="pet_technology_course.php" target="_blank">Click here for more information about this course.</a>
           </p>
+        <?php }
 
-          <?php if ($course[7] == "TRUE") { ?>
-            <p>
-              <a href="pet_technology_course.php" target="_blank">Click here for more information about this course.</a>
-            </p>
-          <?php } ?>
-
+        if ($course_date > $today) { ?>
           <p>
             <a href="<?php echo "documents/" . $course[3]?>" target="_blank">Click here to download the course flyer.</a>
           </p>
           <p>
             <a href="<?php echo $course[4]?>" target="_blank">Click here for more information and to apply.</a>
           </p>
-
-        </div>
+        <?php } ?>
       </div>
-    <?php
-      echo "<hr>";
-    }
+    </div>
+  <?php
+    echo "<hr>";
   }
 } ?>
