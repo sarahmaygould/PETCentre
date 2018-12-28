ece803d8648761b674fc27e8c7959fbc0197d759
Sarah-May Gould <sarahmayg@pet>
2018-12-28 15:53:51 +0000

    Added redirects from old site URLs.

---
+++ about_us.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://sthpetcentre.org.uk/AboutUs_WhatWeDo/what_we_do.php");
+?>
---
+++ patients.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://sthpetcentre.org.uk/ForPatients_YourVisit/your_visit.php");
+?>
