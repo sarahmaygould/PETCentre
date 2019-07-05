f7fac6e3e4a1664c440f2e3fd547a600cfb479ff
Sarah-May Gould <sarahmayg@pet>
2019-07-05 10:28:58 +0100

    Working on automatic ordering of courses.

--- Courses/read_in_courses.php
+++ Courses/read_in_courses.php
@@ -1,17 +1,17 @@
 hello world
 
 <?php
-// $course_array = array_map('str_getcsv', file("courses.csv"));
-// unset($course_array[0]);
-// $today = new DateTime("now");
-//
-// $future_courses = array();
+$course_array = array_map('str_getcsv', file("courses.csv"));
+unset($course_array[0]);
+$today = new DateTime("now");
 
-// foreach ($course_array as $course) {
-//   $course_date = date_create_from_format('d/m/Y', $course[8]);
-//   if ($course_date > $today) {
-//     $future_courses[] = $course;
-//     unset($course_array[$course]);
-//   }
-// }
+$future_courses = array();
+
+foreach ($course_array as $course) {
+  $course_date = date_create_from_format('d/m/Y', $course[8]);
+  if ($course_date > $today) {
+    $future_courses[] = $course;
+    unset($course_array[$course]);
+  }
+}
 ?>
