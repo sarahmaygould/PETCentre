39174c34fb9d7753c6fab06ba4be6c1110098c31
Sarah-May Gould <sarahmayg@pet>
2019-07-05 11:54:29 +0100

    Working on automatic ordering of courses.

--- Courses/read_in_courses.php
+++ Courses/read_in_courses.php
@@ -25,7 +25,43 @@ array_multisort($course_date_array, SORT_ASC, $future_array);
 foreach ($future_array as $course) {
   if ($course[9] == "TRUE") {
 ?>
-<?php echo $course[8]; ?>
+
+    <div class="LR-container">
+      <div class="LI-image">
+        <img src="<?php echo "images/" . $course[2]?>" alt="<?php echo $course[2]?>" width="150">
+      </div>
+      <div class="LI-text">
+        <h1><?php echo $course[0]?></h1>
+        <?php echo $course[1]?>
+
+        <?php if ($course[10] == "TRUE") { ?>
+        <p>
+          <a href="pet_technology_course.php" target="_blank">Click here for more information about this course.</a>
+        </p>
+        <?php } ?>
+
+        <p>
+          This course will next be held on&#47;from the <b><?php echo date_format($course_date, 'jS F Y')?></b>.
+        </p>
+
+        <?php if ($course[12] == "TRUE") { ?>
+          <?php if ($course[3] == "TRUE") { ?>
+          <p>
+            <a href="<?php echo "documents/" . $course[4]?>" target="_blank">Click here to download the flyer for the upcoming course.</a>
+          </p>
+          <?php } ?>
+          <?php if ($course[5] == "TRUE") { ?>
+          <p>
+            <a href="<?php echo "documents/" . $course[6]?>" target="_blank">Click here to download the programme for the upcoming course.</a>
+          </p>
+          <?php } ?>
+          <p>
+            <a href="<?php echo $course[7]?>" target="_blank">Click here for more information and to apply.</a>
+          </p>
+        <?php } ?>
+
+      </div>
+    </div>
 
 <?php }
 } ?>
