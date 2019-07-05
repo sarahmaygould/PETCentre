27ed23898081d0eee7ef56acfffed53922882c6b
Sarah-May Gould <sarahmayg@pet>
2019-07-05 09:50:52 +0100

    Working on automatic ordering of courses.

--- Courses/read_in_courses1.php
+++ Courses/read_in_courses1.php
@@ -1,19 +1,13 @@
 <?php
 $course_array = array_map('str_getcsv', file("courses.csv"));
 unset($course_array[0]);
-$today = new DateTime("now");
-
-$future_courses = array();
 
 foreach ($course_array as $course) {
-  $course_date = date_create_from_format('d/m/Y', $course[8]);
-  if ($course_date > $today) {
-    $future_courses[] = $course;
-    unset($course_array[$course]);
-  }
-}
-?>
+  if ($course[9] == "TRUE") {
 
+    $course_date = date_create_from_format('d/m/Y', $course[8]);
+    $today = new DateTime("now");
+?>
     <div class="LR-container">
       <div class="LI-image">
         <img src="<?php echo "images/" . $course[2]?>" alt="<?php echo $course[2]?>" width="150">
