764b4aa5a9aa15fa45a54309c11e2abd3d461b3a
Sarah-May Gould <sarahmayg@pet>
2019-07-08 13:16:25 +0100

    Sorting out dates to allow multi-date courses.

--- Courses/read_in_courses.php
+++ Courses/read_in_courses.php
@@ -44,15 +44,8 @@ foreach ($future_array as $course) {
         <?php if ($course[13] == "1") {
           $datePhrase = "on the " . date_format($course_date, 'jS F Y');
         } else {
-          $start_date = $course_date;
-          $datePhrase = "from the " . date_format($start_date, 'jS F Y') . " to the " . date_format(date_add($course_date, date_interval_create_from_date_string($course[13] . " days")), 'jS F Y');
+          $datePhrase = "from the " . date_format($course_date, 'jS F Y') . " to the " . date_format(date_add($course_date, date_interval_create_from_date_string($course[13] . " days")), 'jS F Y');
         } ?>
-
-        <?php
-        var_dump($course_date);
-        $enddate = date_add($course_date, date_interval_create_from_date_string($course[13] . " days"));
-        var_dump($enddate);
-        ?>
 
         <p>
           This <?php echo $course[13] ?> day course will next be held <?php echo $datePhrase ?>.
